faa69d2e2810cdb9fa86942c3e8d7ddf4d174056
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-16 08:33:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 15869
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";  
+
+  // accountId = 2;
+
+console.log(accountId);
+
+accountEMail = "hc@gc.com";
+accountPassword = "254163";
+accountCity = "Bengaluru";
+
+console.table([accountEmail, accountPassword, accountCity]);
