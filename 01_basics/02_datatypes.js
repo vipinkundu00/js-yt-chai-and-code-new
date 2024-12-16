b3132059b522cb0f2bcd6f0ae67a4957122a369b
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-16 11:33:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // now all of my code will be treated as newer version of js
+
+ // alert(3+3); // we are using nodejs, not browser
+
+ // code readability should be high 
+
+
+ let name = "victor"; // string data type --> use in double quote ""
+ let age = 28;     // number data type --> 2 to power 53 
+ let isLoggedIn = false;  // boolean data type --> true or false 
+
+ // null  --> standalone value
+ // undefined ---> 
+ // symbol --> used to find uniqueness in react components 
+
+ // --->  Objects <-----
+
+ console.log(typeof null);
+ 
