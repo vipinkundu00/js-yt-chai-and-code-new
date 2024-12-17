07681419af63e282f4663f93d484986354b55183
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-17 16:19:08 +0000

    comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,4 +39,18 @@ let stringNumber = String(someNumber);
 let str1 = "hello";
 let str2 = " hitesh";
 let str3 = str1 + str2;
-console.log(str3);
+ // console.log(str3);
+
+
+// Special cases 
+// console.log("1"+ 3);
+// console.log(1+ "3");
+// console.log("1"+3+5);
+// console.log(1+5+"5"); 
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+//console.log(gameCounter);
