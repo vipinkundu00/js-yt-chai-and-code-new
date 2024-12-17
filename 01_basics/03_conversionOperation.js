1e78a4cabcb6dbea8fc120dd7f6a8a351aa17f84
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-17 04:53:55 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc";
+
+ //console.log(typeof score);
+ // console.log(typeof(score));
+
+let valueInNumber = Number(score);
+ //console.log(typeof valueInNumber);
+ // console.log(valueInNumber);
+
+ // "22" = 22
+ // "33abc" = NaN --> NotANumber
+ // true => 1; false -> 0
+
+let isLoggedIn = "victor";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true ; 0 => false;
+// "" -> false
+// "victor" --> true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
