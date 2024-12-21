ad369bcda25047e35c874cdfdfeefa3d07aa6541
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-21 15:23:21 +0000

    numbers

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,7 +4,7 @@ const repoCount = 100;
 
 // console.log(` Hello my name is ${name} and my repo count is ${repoCount}`); // this is modern way of concatenation and writing syntax
 
-const gameName = new String('hitesh-hc-.com');
+const gameName = new String('hitesh@hc.com');
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 // console.log(gameName.toUpperCase());
@@ -13,6 +13,9 @@ const gameName = new String('hitesh-hc-.com');
 
 // const newString = gameName.substring(0,4);
 // console.log(newString);
+
+const newSplit = gameName.split('--');
+console.log(newSplit);
 
 // const anotherString = gameName.slice(-5,4);
 // console.log(anotherString);
@@ -23,9 +26,9 @@ const gameName = new String('hitesh-hc-.com');
 
 const url = "https://hitesh.com/hitesh%20choudhary";
 
-console.log(url.replace('%20','-'));
-console.log(url.includes('hitesh'));
-console.log(url.includes('alpha'));
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('alpha'));
 
 console.log(gameName.split('-'));
 
--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,7 +1,13 @@
-// const score = 400;
-// console.log(score);
+const score = 400;
+console.log(score);
 
-// const balance = new Number(100);
-// console.log(balance);
+ const balance = new Number(100);
+console.log(balance);
 
 console.log(balance.toString());
+console.log(typeof(balance.toString()));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(3));
