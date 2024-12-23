274a2af1d79a2df77ace8657c0f2f176df599b19
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-23 16:24:42 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,13 +1,41 @@
-const score = 400;
-console.log(score);
+// const score = 400;
+// console.log(score);
 
- const balance = new Number(100);
-console.log(balance);
+//  const balance = new Number(100);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(typeof(balance.toString()));
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(typeof(balance.toString()));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
-const otherNumber = 123.8966;
-console.log(otherNumber.toPrecision(3));
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN')); // Use 'en-IN' for indian number system 
+
+// --------------------> Maths <-----------------------
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.LOG2E); 
+// console.log(Math.abs(-5.63)); // absolute value
+// console.log(Math.ceil(9.2));
+// console.log(Math.floor(5.6)); 
+// console.log(Math.min(5,69,7,9,)); // to find minimum value in an array
+// console.log(Math.max(85,6,3,78,45));  // to find max value in an array
+
+console.log(Math.random()); // value of .random() lies between 0 to 1
+console.log(Math.random()*10);
+
+console.log((Math.random()*10)) +1;
+
+
+
+
+
+
+
+
