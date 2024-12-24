e21c9621ad26335218a2ad0c2f3960659611d846
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-24 11:19:57 +0000

    dates

---
+++ 01_basics/07DatesinJS.js
@@ -0,0 +1,8 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toDateString());
