508a5c1b788470e369bee65461f5ca3261ac8d71
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-25 09:00:44 +0000

    dates

--- 01_basics/07DatesinJS.js
+++ 01_basics/07DatesinJS.js
@@ -1,8 +1,30 @@
 // Dates
 
-let myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toISOString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toDateString());
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+
+// console.log(typeof myDate);
+// let myCreatedDate = new Date(2023,0, 23);
+// let myCreatedDate = new Date(2023,0,23,5,3);
+// let myCreatedDate = new Date("2023-01-12");
+let myCreatedDate = new Date("01-14-2024");
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now()/1000);
+
+let newDate = new Date();
+// console.log(newDate);
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+    
+})
+
