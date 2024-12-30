7dd8a7b450795f07ea319e9bef86a345cfc44c05
vipinkundu00 <167699593+vipinkundu00@users.noreply.github.com>
2024-12-30 09:03:24 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -8,13 +8,20 @@ const dc_heroes = ["superman","flashman", "batman"];
 // console.log(allHeroes);
 
 // const all_new_heroes = [...marvel_heroes, ...dc_heroes];  // this is spread operator(...)
-// console.log(all_new_heroes);  // spread opertor and concatenate method both do same work but spread operator is more useful
+// console.log(all_new_heroes);  // spread opertor(...) and concatenate method both do same work but spread operator is more useful
 
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
 
 // const real_another_array = another_array.flat(Infinity); // Use .flat method to simplify complicated arrays i.e array within an array 
 // console.log(real_another_array);
 
-console.log(Array.isArray("hitesh"));
-console.log(Array.from("hitesh"));
-console.log(Array.from({name: "hitesh"})); // interesting case for interviews
+// console.log(Array.isArray("hitesh"));
+// console.log(Array.from("hitesh"));
+// console.log(Array.from({name: "hitesh"})); // interesting case for interviews
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
